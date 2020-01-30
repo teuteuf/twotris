ecb7cfa81e08bfd171f05c9bfa027dcd1abb531e
TeuTeuf <antoine.teuf@gmail.com>
2020-01-30 14:12:30 +0100

    implement holding on soft drop

--- Assets/Scripts/Controllers.cs
+++ Assets/Scripts/Controllers.cs
@@ -3,24 +3,39 @@ using UnityEngine;
 
 public class Controllers : MonoBehaviour
 {
+    private static readonly Dictionary<PlayerInput, float> HoldingStart = new Dictionary<PlayerInput, float>
+    {
+        {PlayerInput.SoftDrop, 0.0f},
+        {PlayerInput.Left, 0.0f},
+        {PlayerInput.Right, 0.0f}
+    };
+    
     public List<PlayerInput> GetInputs()
     {
         var inputs = new List<PlayerInput>();
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             inputs.Add(PlayerInput.SoftDrop);
+            HoldingStart[PlayerInput.SoftDrop] = Time.time;
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             inputs.Add(PlayerInput.Left);
+            HoldingStart[PlayerInput.SoftDrop] = Time.time;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             inputs.Add(PlayerInput.Right);
+            HoldingStart[PlayerInput.SoftDrop] = Time.time;
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             inputs.Add(PlayerInput.Clockwise);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) && Time.time - HoldingStart[PlayerInput.SoftDrop] > 0.5f)
+        {
+            inputs.Add(PlayerInput.SoftDrop);
         }
 
         return inputs;
