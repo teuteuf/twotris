42a51e867f8f8593fc35b23281c2080aeda709e0
TeuTeuf <antoine.teuf@gmail.com>
2020-02-26 21:51:43 +0100

    improve auto repeat for left and right move

--- Assets/Scripts/Controllers.cs
+++ Assets/Scripts/Controllers.cs
@@ -3,41 +3,64 @@ using UnityEngine;
 
 public class Controllers : MonoBehaviour
 {
-    private static readonly Dictionary<PlayerInput, float> HoldingStart = new Dictionary<PlayerInput, float>
+    public float delayStartAutoRepeat = 0.3f;
+    public float delayMoveAutoRepeat = 0.05f;
+    
+    private static readonly Dictionary<PlayerInput, KeyCode> AutoRepeatableInputs = new Dictionary<PlayerInput, KeyCode>
     {
-        {PlayerInput.SoftDrop, 0.0f},
+        {PlayerInput.Left, KeyCode.LeftArrow},
+        {PlayerInput.Right, KeyCode.RightArrow}
+    };
+    
+    private readonly Dictionary<PlayerInput, float> _holdingStart = new Dictionary<PlayerInput, float>
+    {
         {PlayerInput.Left, 0.0f},
         {PlayerInput.Right, 0.0f}
     };
     
+    private readonly Dictionary<PlayerInput, float> _lastMove = new Dictionary<PlayerInput, float>
+    {
+        {PlayerInput.Left, 0.0f},
+        {PlayerInput.Right, 0.0f}
+    };
+
     public List<PlayerInput> GetInputs()
     {
         var inputs = new List<PlayerInput>();
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             inputs.Add(PlayerInput.SoftDrop);
-            HoldingStart[PlayerInput.SoftDrop] = Time.time;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            inputs.Add(PlayerInput.Left);
-            HoldingStart[PlayerInput.SoftDrop] = Time.time;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            inputs.Add(PlayerInput.Right);
-            HoldingStart[PlayerInput.SoftDrop] = Time.time;
-        }
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             inputs.Add(PlayerInput.Clockwise);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow) && Time.time - HoldingStart[PlayerInput.SoftDrop] > 0.5f)
+        foreach (var autoRepeatableInput in AutoRepeatableInputs)
         {
-            inputs.Add(PlayerInput.SoftDrop);
+            if (Input.GetKeyDown(autoRepeatableInput.Value))
+            {
+                inputs.Add(autoRepeatableInput.Key);
+                _holdingStart[autoRepeatableInput.Key] = Time.time;
+                _lastMove[autoRepeatableInput.Key] = Time.time;
+            }
+            if (IsAutoRepeatActive(autoRepeatableInput.Value, autoRepeatableInput.Key))
+            {
+                inputs.Add(autoRepeatableInput.Key);
+                _lastMove[autoRepeatableInput.Key] = Time.time;
+            }
         }
 
         return inputs;
+    }
+
+    private bool IsAutoRepeatActive(KeyCode key, PlayerInput playerInput)
+    {
+        var isHoldingKey = Input.GetKey(key);
+        var holdKeyForLongEnough = Time.time - _holdingStart[playerInput] > delayStartAutoRepeat;
+        var delayLastMoveElapsed = Time.time - _lastMove[playerInput] > delayMoveAutoRepeat;
+        
+        return isHoldingKey && holdKeyForLongEnough && delayLastMoveElapsed;
     }
 }
